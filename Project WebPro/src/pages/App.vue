543cecf6aca2b84073a79acbe10591432cd5f98e
64070103 Wipupat Chomthaworn <88475411+Wipupat-Chomthaworn@users.noreply.github.com>
2023-04-05 21:08:20 +0700

    Add files via upload

---
+++ Project WebPro/src/pages/App.vue
@@ -0,0 +1,66 @@
+<template>
+  <navbar/>
+  <router-view>
+
+  </router-view>
+</template>
+
+<script>
+import navbar from '../components/Navbar.vue'
+export default {
+  name: 'App',
+  components: {
+    navbar
+  },
+  data () {
+    return {
+    }
+  },
+  methods: {
+  }
+}
+</script>
+
+<style>
+
+</style>
+
+
+
+<!-- <template>
+
+  <NavigationBar></NavigationBar>
+
+<div>
+  <to-do-list></to-do-list>
+</div>
+
+<div> <FirstPage></FirstPage> </div>
+<div><Home></Home></div>
+
+<div>
+
+
+</div>
+</template>
+
+<script>
+import ToDoList from '../components/ToDoList.vue';
+import NavigationBar from '../components/Navigationbar.vue';
+import FirstPage from '../components/FirstPage.vue';
+import Home from './Home.vue';
+
+
+
+
+export default {
+components: {
+  ToDoList,
+  NavigationBar,
+  FirstPage,
+  Home
+},
+};
+</script>
+
+ -->
---
+++ Project WebPro/src/router/index.js
@@ -0,0 +1,48 @@
+import { createRouter, createWebHistory } from "vue-router";
+import Sign from "../pages/Sign.vue";
+import Notfound from "../pages/404.vue";
+// import Calendar from "../pages/Calendar.vue";
+import Profile from "../pages/Profile.vue";
+import Home from "../pages/Home.vue";
+import Start from "../pages/FirstPage.vue";
+import Remaining from "../pages/TaskRemining.vue";
+
+
+
+const routes = [
+  {
+    path: "/",
+    component: Start,
+  },
+  {
+    path: "/sign",
+    component: Sign,
+  },
+  {
+    path: "/:pathMatch(.*)*",
+    component: Notfound,
+  },
+  {
+    path: "/remaining",
+    component: Remaining,
+  },
+  {
+    path: "/Home",
+    component: Home,
+  },
+  {
+    path: "/Profile",
+    component: Profile,
+  },
+  {
+    path: "/Main",
+    component: Profile,
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
