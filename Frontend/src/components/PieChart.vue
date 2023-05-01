d1520d4c34b0ccd75321b99e1c2ca3f3aaab6a53
Owen <88475411+Wipupat-Chomthaworn@users.noreply.github.com>
2023-05-01 20:15:46 +0700

    Revert "Pie chart work but need to parse data from db to them"
    This reverts commit f8bf5db0a834c0a3787e96e9735d5615c5fcc29a.

--- Frontend/src/components/PieChart.vue
+++ Frontend/src/components/PieChart.vue
@@ -1,43 +1,60 @@
-<template>
-   <div>
-     <canvas ref="chart"></canvas>
-   </div>
+<!-- <template>
+    <div>
+       <canvas ref="chart"></canvas>
+    </div>
  </template>
  
  <script>
  import Chart from 'chart.js';
  
  export default {
-   data() {
-     return {
-       chart: null,
-       chartData: {
-         labels: ['Done', 'Todo'],
-         datasets: [
-           {
-             
-             data: [1, 2],
-             backgroundColor: ['#36A2EB', '#FF6384'],
-           }
-         ]
+    mounted() {
+       this.renderChart();
+    },
+    methods: {
+       renderChart() {
+          const ctx = this.$refs.chart.getContext('2d');
+          const chart = new Chart(ctx, {
+             type: 'pie',
+             data: {
+                labels: ['Tasks Completed', 'Tasks Remaining'],
+                datasets: [{
+                   label: 'Tasks',
+                   data: [5, 10],
+                   backgroundColor: ['#36a2eb', '#ff6384']
+                }]
+             }
+          });
        }
-     };
-   },
-   mounted() {
-     // Initialize chart with mock data
-     this.chart = new Chart(this.$refs.chart, {
-       type: 'pie',
-       data: this.chartData
-     });
- 
-     // Fetch actual data from backend and update chart
-     axios.get('/api/tasks').then(response => {
-       const doneCount = response.data.filter(task => task.status === 'done').length;
-       const leftCount = response.data.filter(task => task.status === 'left').length;
-       this.chartData.datasets[0].data = [doneCount, leftCount];
-       this.chart.update();
-     });
-   }
- };
+    }
+ }
  </script>
- 
+  -->
+
+
+  <template>
+    <div>
+      <canvas ref="chart"></canvas>
+    </div>
+  </template>
+  
+  <script>
+  import { Pie } from 'vue-chartjs';
+  
+  export default {
+    extends: Pie,
+    props: {
+      data: {
+        type: Object,
+        required: true
+      }
+    },
+    mounted() {
+      this.renderChart(this.data, {
+        responsive: true,
+        maintainAspectRatio: false
+      });
+    }
+  };
+  </script>
+  
