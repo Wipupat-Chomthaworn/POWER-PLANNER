bf37c7000e2f25d452bfdd3935fcefa4a60f96ed
Owen <88475411+Wipupat-Chomthaworn@users.noreply.github.com>
2023-05-18 21:44:36 +0700

    Create user_info.sql

---
+++ Frontend/src/components/user_info.sql
@@ -0,0 +1,20 @@
+INSERT INTO `webpro`.`user_info`
+(`user_id`,
+`email`,
+`username`,
+`password`,
+`first_name`,
+`last_name`,
+`phone`,
+`user_type`,
+`pref_id`)
+VALUES
+(<{user_id: }>,
+<{email: }>,
+<{username: }>,
+<{password: }>,
+<{first_name: }>,
+<{last_name: }>,
+<{phone: }>,
+<{user_type: customer}>,
+<{pref_id: }>);
