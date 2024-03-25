de0d5b61bb249a68e10fc2163f30f72abcd8a173
64070103 Wipupat Chomthaworn <88475411+Wipupat-Chomthaworn@users.noreply.github.com>
2024-03-25 16:52:00 +0700

    Update axios.js

--- Frontend/src/plugins/axios.js
+++ Frontend/src/plugins/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    baseURL: `http://34.143.231.153/:3000`,
+    baseURL: `http://34.143.231.153:3000`,
     headers: {
         'Referrer-Policy': 'no-referrer', // Or any other desired policy
       },
